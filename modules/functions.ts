c78a082f956affa34a92b893000a20321347af60
mgkkawa <k.kawate@mg-k.co.jp>
2022-11-16 00:02:58 +0900

    test

--- modules/functions.ts
+++ modules/functions.ts
@@ -1,3 +1,5 @@
+import { DELETE_KEYS } from '../03-callhistory/src/modules'
+
 export const checkGuestSpace = async () => {
   const currentUrl = kintone.api.url('/k/v1/app', true)
   if (currentUrl.indexOf('guest') > -1) {
@@ -34,13 +36,23 @@ export const getAppInfo = async appId => {
   })
 }
 
+export const getApps = async appId => {
+  const isGuest = Boolean(await checkGuestSpace())
+  const appInfo = await kintone.api(kintone.api.url('/k/v1/app', isGuest), 'GET', { id: Number(appId) })
+  const spaceId = appInfo.spaceId
+  const url = kintone.api.url('/k/v1/space', isGuest)
+
+  const response = await kintone.api(url, 'GET', { id: spaceId })
+  return response.attachedApps
+}
+
 export const getConfig = id => {
-  const config = kintone.plugin.app.getConfig(id)
+  let config = kintone.plugin.app.getConfig(id)
 
   for (let key in config) {
     const value = config[key]
     try {
-      config[key] = JSON.parse(value)
+      config[key] = parse(value)
     } catch {
       continue
     }
@@ -53,11 +65,63 @@ export const checkConfig = config => {
   for (let key in config) {
     const value = config[key]
     try {
-      config[key] = JSON.parse(value)
+      config[key] = JSON.stringify(value)
     } catch {
-      continue
+      config[key] = String(value)
     }
   }
 
   return config
+}
+
+export const getUnicode = str => {
+  const array = str.split('')
+  const res = array.map(value => value.codePointAt(0))
+}
+
+export const getItems = (fields, values = []) => {
+  const items = []
+
+  for (let key in fields) {
+    const field = fields[key]
+    if (DELETE_KEYS.includes(field.type) || values.includes(field.code)) continue
+    items.push({ label: field.label, value: field.code })
+  }
+  items.sort((a, b) => {
+    if (!a.label || !b.label) return true
+    const _a = a.label.split('').map(value => value.codePointAt(0))
+    const _b = b.label.split('').map(value => value.codePointAt(0))
+
+    let check
+    let result
+    const array = _a
+    const target = _b
+
+    array.forEach((num, index) => {
+      if (check || !target[index] || num == target[index]) return
+      result = num - target[index]
+      check = true
+    })
+
+    return result
+  })
+  return items
+}
+
+export const parse = s => {
+  // preserve newlines, etc - use valid JSON
+  s = s
+    .replace(/\\n/g, '\\n')
+    .replace(/\\'/g, "\\'")
+    .replace(/\\"/g, '\\"')
+    .replace(/\\&/g, '\\&')
+    .replace(/\\r/g, '\\r')
+    .replace(/\\t/g, '\\t')
+    .replace(/\\b/g, '\\b')
+    .replace(/\\f/g, '\\f')
+
+  // remove non-printable and other non-valid JSON chars
+  s = s.replace(/[\u0000-\u0019]+/g, '')
+
+  return JSON.parse(s)
 }
