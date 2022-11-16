c470874132c46757dbebb86346f1f5853617816c
mgkkawa <k.kawate@mg-k.co.jp>
2022-11-16 23:36:38 +0900

    履歴保存プラグイン

--- 03-callhistory/src/ts/desktop.ts
+++ 03-callhistory/src/ts/desktop.ts
@@ -5,6 +5,44 @@ jQuery.noConflict()
 ;(async ($, PLUGIN_ID) => {
   'use strict'
 
+  kintone.events.on('app.record.edit.show', async event => {
+    const record = event.record
+
+    const config = await plugins.getConfig(PLUGIN_ID)
+    for (let key in config) {
+      if (typeof config[key] === 'string') config[key] = parse(config[key])
+    }
+
+    const SERIAL_NO = record.法人番号.value
+    const CASE_NAME = record.案件名.value
+
+    const history = config.history
+    const appId = history.appId
+    const token = history.token
+
+    const endpoint = kintone.api.url('/k/v1/records', true)
+    const query = `法人番号 = "${SERIAL_NO}" and 案件名 = "${CASE_NAME}" and inout = "out" order by レコード番号 asc`
+    const url = `${endpoint}?app=${appId}&query=${encodeURI(query)}&totalCount=true`
+    const headers = {
+      'X-Cybozu-API-Token': token,
+      // 'Content-Type': 'application/json',
+    }
+
+    const count = await kintone
+      .proxy(url, 'GET', headers, {})
+      .then(resp => {
+        const object = JSON.parse(resp[0])
+        return object.totalCount - 0
+      })
+      .catch(resp => {
+        console.log(resp)
+        return 0
+      })
+
+    record.コール数.value = count + 1
+    return event
+  })
+
   kintone.events.on(['app.record.create.submit.success', 'app.record.edit.submit.success'], async event => {
     const record = event.record
     if (record['編集ステータス'].value) return
@@ -14,8 +52,9 @@ jQuery.noConflict()
       if (typeof config[key] === 'string') config[key] = parse(config[key])
     }
 
-    const historyId = config.history.appId
-    const token = config.history.token
+    const history = config.history
+    const appId = history.appId
+    const token = history.token
 
     const fields = config.fields2
     const body = {}
@@ -30,7 +69,7 @@ jQuery.noConflict()
     }
 
     const param = {
-      app: historyId,
+      app: appId,
       record: body,
     }
 
